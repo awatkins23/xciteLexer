32156229141f91549cb3c4a19e60f9f3210abcd4
Cwdearth <77296394+Cwdearth@users.noreply.github.com>
2024-02-20 18:12:38 -0600

    More progress
    This version runs

--- handlingProcesses.py
+++ handlingProcesses.py
@@ -1,34 +1,38 @@
-import sys
+import time
 
-separators = ["==" , "<=" , ">=" , "[" , "]" , "=" , "," , "!" , ">",
-              "<" , "(" , ")" , "{" , "}" , "+" , "-" , "/" , "*" , "%"]
-
-keywords = ["PRINT","INT","BOOL", "FLOAT", "CHAR", "STRING", "ARRAY",
+keywordSeparators = ["<=" , ">=" , "==" , "[" , "]" , "=" , "," , "!" , ">",
+              "<" , "(" , ")" , "{" , "}" , "+" , "-" , "/" , "*" , "%",
+              "PRINT","INT","BOOL", "FLOAT", "CHAR", "STRING", "ARRAY",
             "WHILE","IF", "ELSE", "TRUE", "FALSE", "NOT", "AND", "OR"]
 
 subStrings = []
-currentStartIndex = 0
+currentProgram = ""
+start_time = 0
 
 
 def readArgs(pathArgs):
+    global currentProgram
+    global start_time
 
-    for arg in pathArgs:
+    for x in range(1, len(pathArgs)):
         try:
-            file = open(arg, "r")
+            file = open(pathArgs[x], "r")
+            currentProgram = pathArgs[x]
         except:
-            print(f'File not found at \"{arg}\"')
+            print(f'File not found at \"{pathArgs[x]}\"')
             exit()
+        start_time = time.time()
         lex(file)
 
 def divideStringsFromToken(subStringIndex, startOfToken, endOfToken):
 
-    leftSubString = subStrings[subStringIndex][1][0:startOfToken]
-    rightSubString = subStrings[subStringIndex][1][endOfToken:-1]
-    tokenSubString = subStrings[subStringIndex][1][startOfToken:endOfToken]
+    leftSubString = subStrings[subStringIndex][1][0:startOfToken].strip()
+    rightSubString = subStrings[subStringIndex][1][endOfToken:len(subStrings[subStringIndex][1])].strip()
+    tokenSubString = subStrings[subStringIndex][1][startOfToken:endOfToken].strip()
 
     if(leftSubString != ""):
-        subStrings.insert(subStringIndex, (False, leftSubString))
         del subStrings[subStringIndex]
+        subStrings.insert(subStringIndex, (False, leftSubString))
         subStrings.insert(subStringIndex + 1, (True, tokenSubString))
         if(rightSubString != ""):
             subStrings.insert(subStringIndex + 2, (False, rightSubString))
@@ -42,41 +46,105 @@ def quoteParser(line):
 
     subStringIndex = subStrings.index((False, line))
     subString = subStrings[subStringIndex][1]
-
     double_index = line.find("\"")
     single_index = line.find("\'")
 
     if ((double_index & single_index) == -1):
         return
+    
     elif ((double_index | single_index) != -1):
         startOfToken = min(double_index, single_index)
         endOfToken = subString.find(subString[startOfToken], startOfToken + 1)
+
     else:
         startOfToken = max(double_index, single_index)
         endOfToken = subString.find(subString[startOfToken], startOfToken + 1)
 
-    print(f"start: {startOfToken} end: {endOfToken} subString = {subString}" )
-
     if(endOfToken == -1):
         divideStringsFromToken(subStringIndex, startOfToken, -1)
+
     else:
-        divideStringsFromToken(subStringIndex, startOfToken, endOfToken)
-        # RECURSE THE RIGHT SIDE
-        if(subStringIndex <= len(subStrings)):
-            quoteParser(subStrings[subStringIndex + 1])
+        divideStringsFromToken(subStringIndex, startOfToken, endOfToken + 1)
+
+        if(subStringIndex + 2 < len(subStrings)):
+            quoteParser(subStrings[subStringIndex + 2][1])
+
+def keywordSeperatorParser():
+    for token in keywordSeparators:
+        subStringsLength = len(subStrings) - 1
+
+        for currentIndex in range(0,subStringsLength + 1):
+            subStringsLength = len(subStrings)
+
+            if subStrings[currentIndex][0] != True:
+                startOfToken = subStrings[currentIndex][1].find(token)
+
+                if(startOfToken != -1):
+                    endOfToken = startOfToken + len(token)
+                    divideStringsFromToken(currentIndex, startOfToken, endOfToken)
+
+def indentifierParser():
+    subStringsLength = len(subStrings)
+    subStringsLength = len(subStrings)
+    currentIndex = 0
+
+    while (currentIndex < subStringsLength):
+        if subStrings[currentIndex][0] != True:
+            startOfToken = subStrings[currentIndex][1].find("@")
+
+            if (startOfToken == -1):
+                currentIndex += 1
+                continue
+
+            subString = subStrings[currentIndex][1]
+            
+            if (startOfToken == len(subString) - 1):
+                divideStringsFromToken(currentIndex, startOfToken, startOfToken + 1)
+                currentIndex += 1
+            else:
+                x = startOfToken + 1
+                while (((subString[x] == "_") or (subString[x].islower()))):
+                        if(subString[x] == "@"):
+                            divideStringsFromToken(currentIndex, startOfToken, x - 1)
+                            break
+                        if(x == len(subString) - 1):
+                            x+=1
+                            break
+                        x+=1
+
+                endOfToken = x
+
+                divideStringsFromToken(currentIndex, startOfToken, endOfToken)
+                currentIndex += 1
+        else:
+            currentIndex += 1
+        subStringsLength = len(subStrings)
 
 def lex(file):
 
-    global currentStartIndex
     global subStrings
 
-    currentStartIndex = 0
     subStrings.clear()
     program = file.readlines()
 
     for line in program:
         
-        subStrings.append((False, line))
+        line = line.strip()
 
+        if(line == ""):
+            continue
+
+
+        subStrings.append((False, line))
         quoteParser(line)
-    print(subStrings)
+
+        # MUST DEAL WITH COMMENTS RIGHT AFTER QUOTE
+
+        keywordSeperatorParser()
+        indentifierParser()
+
+    end_time = time.time()
+    print(f'     TOKENIZATION FOR "{currentProgram}" TOOK {end_time - start_time}\n')
+    for string in subStrings:
+        print(f"{string[0]} : {string[1]}")
+    
